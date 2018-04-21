2d023d64f53c3341c90e96b9e897880cde6c0fd8
Sam-PC\Sam <sonicbdp@gmail.com>
2018-04-21 17:13:28 +1000

    First upload

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+	
+	public void GameScene() 
+	{
+		SceneManager.LoadScene(1);		//Loads the game scene
+	}
+
+	public void CreditScene()
+	{
+		SceneManager.LoadScene(2);		//Loads the credit scene
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			rb.AddForce (Vector3.left * 20);
+			Debug.Log ("Moving Left");
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce (Vector3.right * 75);
+			Debug.Log ("Moving Right");
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rb.velocity = Vector3.up * 7;
+			Debug.Log ("Jump");
+		}
+	}
+}
